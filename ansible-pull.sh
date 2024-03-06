3276814bd7dc965965702a9177488835d373ba2b
Giorgi Zeikidze <giorgi.zeikidze@telia.ee>
2024-03-06 12:40:02 +0200

    set python version

--- ansible-pull.sh
+++ ansible-pull.sh
@@ -1,6 +1,6 @@
 apt update && apt install python3.9-venv -y
 # Create a virtual environment named "ansible-venv" in the home folder
-python3 -m venv ~/ansible-venv
+python3.9 -m venv ~/ansible-venv
 
 # Activate the virtual environment
 source ~/ansible-venv/bin/activate
