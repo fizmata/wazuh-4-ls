941700f9b41ce2a90688071ab7e93549922527c4
Giorgi Zeikidze <giorgi.zeikidze@telia.ee>
2024-03-06 12:42:13 +0200

    simpler script, install in /opt/

--- ansible-pull.sh
+++ ansible-pull.sh
@@ -1,9 +1,9 @@
 apt update && apt install python3.9-venv -y
 # Create a virtual environment named "ansible-venv" in the home folder
-python3.9 -m venv ~/ansible-venv
+python3.9 -m venv /opt/ansible-venv
 
 # Activate the virtual environment
-source ~/ansible-venv/bin/activate
+source /opt/ansible-venv/bin/activate
 
 # Install Ansible within the virtual environment
 pip install ansible
@@ -11,14 +11,5 @@ pip install ansible
 # Deactivate the virtual environment
 deactivate
 
-# Create aliases for Ansible binaries in the virtual environment
-for file in ~/ansible-venv/bin/*ansible*; do
-    binary_name=$(basename "$file")
-    echo "alias $binary_name=\"$file\"" >> ~/.bashrc
-done
-
-# Reload the bashrc file to apply the aliases
-source ~/.bashrc
-
-ansible-pull -U https://github.com/fizmata/wazuh-4-ls -d /tmp/ansible-pull -i localhost, local.yaml
+/opt/ansible-venv/bin/ansible-pull -U https://github.com/fizmata/wazuh-4-ls -d /tmp/ansible-pull -i localhost, local.yaml
 
