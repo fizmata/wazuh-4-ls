b49276438a07773142ec9d686a1abf738044de66
Giorgi Zeikidze <giorgi.zeikidze@telia.ee>
2024-03-06 11:48:44 +0200

    add pull script and other cool things

---
+++ ansible-pull.sh
@@ -0,0 +1,23 @@
+# Create a virtual environment named "ansible-venv" in the home folder
+python3 -m venv ~/ansible-venv
+
+# Activate the virtual environment
+source ~/ansible-venv/bin/activate
+
+# Install Ansible within the virtual environment
+pip install ansible
+
+# Deactivate the virtual environment
+deactivate
+
+# Create aliases for Ansible binaries in the virtual environment
+for file in ~/ansible-venv/bin/*ansible*; do
+    binary_name=$(basename "$file")
+    echo "alias $binary_name=\"$file\"" >> ~/.bashrc
+done
+
+# Reload the bashrc file to apply the aliases
+source ~/.bashrc
+
+ansible-pull -U https://github.com/fizmata/wazuh-4-ls -d /tmp/ansible-pull -i localhost, local.yaml
+
